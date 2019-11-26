89afe0e8fafb0f4d3a53fa9e688fee21464a4118
mathi <mathiaswrc@gmail.com>
2019-11-26 19:11:47 -0300

    criado mk-teste.html

---
+++ main.js
@@ -0,0 +1,35 @@
+var brand = new Vue({
+    el:'#brand',
+    data: {
+        logo: 'img/logo-mk.jpg',
+    }
+}) 
+
+var carousel = new Vue({
+    el:'#carousel',
+    data: {
+        banner1: 'img/banner-bf.jpg',
+        banner2: 'img/banner-bf.jpg',
+        banner3: 'img/banner-bf.jpg',
+    }
+}) 
+
+var line1 = new Vue({
+    el:'#cd-line-1',
+    data: {
+        cd1: 'img/Midian-Lima.jpg',
+        cd2: 'img/Kleber-Lucas.jpg',
+        cd3: 'img/Elaine-Jesus.jpg',
+        cd4: 'img/Marine-Friesen.jpg',
+    }
+})
+
+var line2 = new Vue({
+    el:'#cd-line-2',
+    data: {
+        cd5: 'img/Joao-Marcos.jpg',
+        cd6: 'img/Vaneyse.jpg',
+        cd7: 'img/Elaine-Martins.jpg',
+        cd8: 'img/Bekah-Costa.jpg'
+    }
+})
